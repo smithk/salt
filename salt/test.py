6dcc35c8133e111491a087932991a46367ab7e3d
Roger Bermudez-Chacon <rogerberm@gmail.com>
2014-05-05 19:26:49 +0200

    test.py updated

--- salt/test.py
+++ salt/test.py
@@ -1,16 +1,5 @@
-"""Test suite for SALT."""
-
-import unittest
-from salt._unittests.options_readers import OptionsTests
-from salt._unittests.IO_readers import IOTests
-
-def suite():
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(OptionsTests))
-    suite.addTest(unittest.makeSuite(IOTests))
-    return suite
+from salt.IO import readers
 
 if __name__ == '__main__':
-    test_suite = suite()
-    runner = unittest.TextTestRunner()
-    runner.run(test_suite)
+    args = "salt/code/data/standard_ml_sets/classification/wine.arff"
+    a = readers.ArffReader(args)
